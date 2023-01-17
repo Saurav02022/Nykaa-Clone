ce6578b0028576c7ecfe717de2f0ce354e61c446
Raghavendra <raghavendrardeshabhandari@gmail.com>
2023-01-17 19:22:24 +0530

    fw20_0635_footer added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,7 @@
 import { Footer } from "./Components/Footer/Footer";
 
 function App() {
-  return <div>Nykaa.com
-    <Footer />
-  </div>;
+  return <div>Nykaa.com</div>;
 }
 
 export default App;
