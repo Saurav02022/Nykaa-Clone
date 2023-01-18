d1072e99c7879bc5f7dd354da87cbf54aafba99a
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-01-18 20:44:58 +0530

    admin backend

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+const { application } = require('express')
+const express = require('express')
+const { connection } = require('./Admin/Config/db')
+const { adminRouter } = require('./Admin/Controllers/admin.routes')
+const { faceRouter } = require('./Admin/Controllers/face.routes')
+const { skinRouter } = require('./Users/Controllers/skin.routes')
+const { userRouter } = require('./Users/Controllers/users.routes')
+
+const app=express()
+
+app.use(express.json())
+app.use('/users',userRouter)
+app.use("/admins",adminRouter)
+app.use("/face",faceRouter)
+app.use("/skin",skinRouter)
+
+app.listen('5000',async(req,res)=>{
+    try{
+        await connection
+        console.log("Connected to db")
+    }catch(err){
+        console.log(err)
+    }
+    console.log("Running on port 5000")
+})
