15a83293ccc1b632dd4b1188c90fd10a2b98e326
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-01-19 22:12:26 +0530

    admin-fe

--- Frontend/src/routes/routers.js
+++ Frontend/src/routes/routers.js
@@ -5,17 +5,25 @@ import { Footer } from "../Components/Footer/footer.jsx";
 import CartPage from "../Pages/CartPage";
 import FaceProductPage from "../Pages/FaceProductPage";
 import SkinProductPage from "../Pages/SkinProductPage";
+import Login from "../AdminFE/Pages/Login";
+import Panel from "../AdminFE/Pages/Panel";
+import Products from "../AdminFE/Pages/Products";
+import Users from "../AdminFE/Pages/Users";
 const Routers = () => {
   return (
     <>
-      <Navbar />
+   
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/admin/login" element={<Login />} />
+        <Route path="/admin/panel" element={<Panel />} />
+        <Route path="/admin/products" element={<Products />} />
+        <Route path="/admin/users" element={<Users />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/face" element={<FaceProductPage/>} />
         <Route path="/skin" element={<SkinProductPage />}/>
       </Routes>
-      <Footer />
+    
     </>
   );
 };
