3a1fa4dcae778f8046b98b13c3de2e1428fa166b
aditya ghosh <adityasinghghosh36@gmail.com>
2023-01-19 23:26:53 +0530

    Login-signup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,11 @@
-import { Footer } from "./Components/Footer/Footer";
+// import { Footer } from "./Components/Footer/Footer";
+import { MainRoutes } from "./Pages/mainRoutes";
 
 function App() {
-  return <div>Nykaa.com</div>;
+  return <div>
+    {/* Nykaa.com */}
+  <MainRoutes />
+  </div>;
 }
 
 export default App;
