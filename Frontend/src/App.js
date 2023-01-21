ffff777b5c0dd72efe4c278dd426f90282eaafcb
Raghavendra <raghavendrardeshabhandari@gmail.com>
2023-01-21 22:04:44 +0530

    day-5 files added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,10 @@
-import SingleProduct from "./Pages/SingleProductPage/SingleProduct";
 import AdminRoutes from "./routes/admin.routes";
 import UserRoutes from "./routes/user.routes";
 function App() {
   return (
     <div>
-      {/* <UserRoutes />
-      <AdminRoutes /> */}
-      <SingleProduct />
+      <UserRoutes />
+      <AdminRoutes />
     </div>
   );
 }
