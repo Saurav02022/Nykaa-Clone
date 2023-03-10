fab7a7c69bb7ebd32e4ed6aac8182ddf7f18ecbe
SauravKumar <sk729584@gmail.com>
2023-03-10 21:54:14 +0530

    update redux

--- Frontend/src/Redux/store.js
+++ Frontend/src/Redux/store.js
@@ -6,16 +6,19 @@ import {
 } from "redux";
 
 import { AuthenticationReducer } from "./LogReducer/reducer";
-import { CartReducer } from "./CartPage/reducer";
-import { AddressReducer } from "./AddressPage/reducer";
+import { CartReducer } from './CartPage/reducer'
+import {AddressReducer} from './AddressPage/reducer'
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   AuthenticationReducer,
   CartReducer,
-  AddressReducer,
+  AddressReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+export const store = legacy_createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
