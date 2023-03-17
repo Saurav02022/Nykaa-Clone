f904266079f67383c237b0c033b5fc2fa66338cc
SauravKumar <sk729584@gmail.com>
2023-03-18 00:59:31 +0530

    remove middleware of redux

--- Frontend/src/Redux/store.js
+++ Frontend/src/Redux/store.js
@@ -16,8 +16,8 @@ const rootReducer = combineReducers({
   AddressReducer,
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = compose;
 export const store = legacy_createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
