ea8003abca9c66d7a8d2530db2eec5bbb504be17
unknown <mdali91935@gmail.com>
2023-03-17 23:07:32 +0530

    update css in prod page

--- Frontend/src/Pages/FaceProductPage/index.js
+++ Frontend/src/Pages/FaceProductPage/index.js
@@ -12,7 +12,7 @@ import Pagination from "../Pagination";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Loading from "../Loading";
-import { Box,Center } from "@chakra-ui/react";
+import { Box, Center } from "@chakra-ui/react";
 
 
 
@@ -69,28 +69,28 @@ const banner = [
 
 function FaceProductPage() {
 
-  const [data,setData]=useState([])
+  const [data, setData] = useState([])
 
-  const [loading,setLoading] = useState(false)
-  const [error,setError] = useState(false)
-  const [page,setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [page, setPage] = useState(1)
 
-  const getData=(page)=>{
-    
+  const getData = (page) => {
+
     setLoading(true)
-    axios.get("https://fair-pear-salmon-suit.cyclic.app/face",{
-      params:{
-        _limit:12,
-        _page:page
+    axios.get("https://fair-pear-salmon-suit.cyclic.app/face", {
+      params: {
+        _limit: 12,
+        _page: page
       }
-    }).then((res)=>{
+    }).then((res) => {
       setTimeout(() => {
         setLoading(false)
         setData(res.data)
-        }, 2000);
-    
+      }, 2000);
+
       // console.log(res.data)
-    }).catch((err)=>{
+    }).catch((err) => {
       console.log(err)
       setError(true)
     })
@@ -98,117 +98,117 @@ function FaceProductPage() {
 
 
 
-  useEffect(()=>{
+  useEffect(() => {
     getData(page)
-  },[page])
+  }, [page])
 
-  
-  const pricelh=()=>{
+
+  const pricelh = () => {
     setLoading(true)
     axios.get("https://fair-pear-salmon-suit.cyclic.app/face/priceasc")
-    .then((res)=>{
-      setData(res.data)
-    setLoading(false)
-      // console.log(res.data)
-    }).catch((err)=>{
-      console.log(err)
-    setError(true)
+      .then((res) => {
+        setData(res.data)
+        setLoading(false)
+        // console.log(res.data)
+      }).catch((err) => {
+        console.log(err)
+        setError(true)
 
-    })
+      })
   }
 
-  const pricehl=()=>{
+  const pricehl = () => {
     setLoading(true)
     axios.get("https://fair-pear-salmon-suit.cyclic.app/face/pricedesc")
-    .then((res)=>{
-      setData(res.data)
-    setLoading(false)
+      .then((res) => {
+        setData(res.data)
+        setLoading(false)
 
-      // console.log(res.data)
-    }).catch((err)=>{
-      console.log(err)
-    setError(true)
+        // console.log(res.data)
+      }).catch((err) => {
+        console.log(err)
+        setError(true)
 
-    })
+      })
   }
 
 
-  const ratinglh=()=>{
+  const ratinglh = () => {
     setLoading(true)
     axios.get("https://fair-pear-salmon-suit.cyclic.app/face/ratingasc")
-    .then((res)=>{
-      setData(res.data)
-    setLoading(false)
-      // console.log(res.data)
-    }).catch((err)=>{
-      console.log(err)
-    setError(true)
+      .then((res) => {
+        setData(res.data)
+        setLoading(false)
+        // console.log(res.data)
+      }).catch((err) => {
+        console.log(err)
+        setError(true)
 
-    })
+      })
   }
 
-  const ratinghl=()=>{
+  const ratinghl = () => {
     setLoading(true)
     axios.get("https://fair-pear-salmon-suit.cyclic.app/face/ratingdesc")
-    .then((res)=>{
-      setData(res.data)
-    setLoading(false)
-      // console.log(res.data)
-    }).catch((err)=>{
-      console.log(err)
-    setError(true)
+      .then((res) => {
+        setData(res.data)
+        setLoading(false)
+        // console.log(res.data)
+      }).catch((err) => {
+        console.log(err)
+        setError(true)
 
-    })
+      })
   }
 
 
 
-    const handlePriceChange=(e)=>{
-      if(e.target.value==="Price low to high"){
-        pricelh()
-      }else if(e.target.value === "Price high to low"){
-        pricehl()
-      }
-      
-    }
-    const handleRatingChange=(e)=>{
-      if(e.target.value==="Rating low to high"){
-     ratinglh()
-      }else if(e.target.value === "Rating high to low"){
-     ratinghl()
-      }
-      
+  const handlePriceChange = (e) => {
+    if (e.target.value === "Price low to high") {
+      pricelh()
+    } else if (e.target.value === "Price high to low") {
+      pricehl()
     }
 
-    const reset=()=>{
-      setLoading(true)
-      setTimeout(() => {
-        setLoading(false)
-      }, 2000);
-      getData();
-     let ptag= document.getElementById("priceSort")
-      ptag.value=""
-      let rtag=document.getElementById("ratingSort")
-       rtag.value=""
-       let ctag=document.getElementById("categorySort")
-       ctag.value=""
-       setFilter("")
+  }
+  const handleRatingChange = (e) => {
+    if (e.target.value === "Rating low to high") {
+      ratinglh()
+    } else if (e.target.value === "Rating high to low") {
+      ratinghl()
     }
-    
-    const [filter,setFilter]=useState("")
-    const category=(e)=>{
+
+  }
+
+  const reset = () => {
     setLoading(true)
-      let filterData= data.filter
-      ((el)=> {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000);
+    getData();
+    let ptag = document.getElementById("priceSort")
+    ptag.value = ""
+    let rtag = document.getElementById("ratingSort")
+    rtag.value = ""
+    let ctag = document.getElementById("categorySort")
+    ctag.value = ""
+    setFilter("")
+  }
+
+  const [filter, setFilter] = useState("")
+  const category = (e) => {
+    setLoading(true)
+    let filterData = data.filter
+      ((el) => {
         return el.title.indexOf(e.target.value) !== -1
-        ? true:false;
+          ? true : false;
       }).map((el) => el);
     setLoading(false)
-      setFilter(filterData)
+    setFilter(filterData)
 
-    }
+  }
 
-    
+
 
 
 
@@ -239,8 +239,8 @@ function FaceProductPage() {
             <h2 style={{ fontSize: "25px", fontWeight: "500", textAlign: "center", marginBottom: "5px" }}>Face Makeup Collection</h2>
           </div>
           <Slider {...settings} >
-            {banner.map((el) => (
-              <div className="popup" key={el}>
+            {banner.map((el, i) => (
+              <div className="popup" key={i}>
                 <img className="bnr_img" src={el} alt="banner" />
               </div>
             ))}
@@ -273,21 +273,21 @@ function FaceProductPage() {
         <div className="product_containor">
           <div className="sort">
 
-          <button onClick={reset}>Reset : </button>
-           
-            <select onChange={(e)=>handlePriceChange(e)} id="priceSort" name="Price">
+            <button onClick={reset}>Reset : </button>
+
+            <select onChange={(e) => handlePriceChange(e)} id="priceSort" name="Price">
               <option value="">Price</option>
               <option value="Price low to high">Price low to high</option>
               <option value="Price high to low">Price high to low</option>
             </select>
 
-            <select onChange={(e)=>handleRatingChange(e)}  id="ratingSort" name="Rating">
+            <select onChange={(e) => handleRatingChange(e)} id="ratingSort" name="Rating">
               <option value="">Rating</option>
               <option value="Rating low to high">Rating low to high</option>
               <option value="Rating high to low">Rating high to low</option>
             </select>
 
-            <select onChange={(e)=>category(e)} id="categorySort" name="Category">
+            <select onChange={(e) => category(e)} id="categorySort" name="Category">
               <option value="">Category</option>
               <option value="Maybelline">Maybelline </option>
               <option value="Kay ">Kay </option>
@@ -297,14 +297,14 @@ function FaceProductPage() {
             </select>
 
             {/* <button onClick={category}>nyc</button> */}
-                        
+
           </div>
-          {loading && <Loading/>}
-      {error && (<Box>
-        <Center>
-        <img width="150px" src="https://www.seekpng.com/png/full/360-3605845_dog-holding-paper-in-mouth.png" alt="" />
-        </Center>
-        </Box>)}
+          {loading && <Loading />}
+          {error && (<Box>
+            <Center>
+              <img width="150px" src="https://www.seekpng.com/png/full/360-3605845_dog-holding-paper-in-mouth.png" alt="" />
+            </Center>
+          </Box>)}
           <Flex
             display={"grid"}
             gridTemplateColumns={{
@@ -320,7 +320,7 @@ function FaceProductPage() {
             marginTop="20px"
             marginBottom={"20px"}
           >
-               {filter.length>0 ? filter.map(
+            {filter.length > 0 ? filter.map(
               ((el) => (
                 <ProductCartItem key={el._id}
                   imgsrc={el.imgsrc}
@@ -331,32 +331,34 @@ function FaceProductPage() {
                   rating={el.rating}
                 />
               )
-            )) :
-            data.length>0 && data.map(
-              ((el) => (
-                <ProductCartItem key={el._id}
-                  _id={el._id}
-                  imgsrc={el.imgsrc}
-                  title={el.title}
-                  price={el.price}
-                  discount={el.discount}
-                  discountedprice={el.discountedprice}
-                  rating={el.rating}
-                />
-              )
-            ))}
+              )) :
+              data.length > 0 && data.map(
+                ((el) => (
+                  <ProductCartItem key={el._id}
+                    _id={el._id}
+                    imgsrc={el.imgsrc}
+                    title={el.title}
+                    price={el.price}
+                    discount={el.discount}
+                    discountedprice={el.discountedprice}
+                    rating={el.rating}
+                  />
+                )
+                ))}
           </Flex>
           <div className='page-btn'>
-          <button disabled={page<2} onClick={() => setPage(page - 1)}>Prev</button>
-            <button>{page}</button>
-            <button  onClick={() => setPage(page + 1)}>Next</button>
-          </div>
+          <button disabled={page < 2} onClick={() => setPage(page - 1)}>Prev</button>
+          <button>{page}</button>
+          <button onClick={() => setPage(page + 1)}>Next</button>
+        </div>
+
 
         </div>
+      
 
       </div>
 
-      
+
 
 
     </div>
